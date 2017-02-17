19933ad523a74b8c88338e296c9ef5d7a348e75e
Andy Atkinson <andyatkinson@gmail.com>
2017-02-16 20:40:27 -0600

    delete from bucket by key

--- kv/test/kv/bucket_test.exs
+++ kv/test/kv/bucket_test.exs
@@ -1,11 +1,24 @@
 defmodule KV.BucketTest do
   use ExUnit.Case, async: true # uses multiple cores
 
-  test "stores values by key" do
+  # setup callback, runs before every test
+  setup do
     {:ok, bucket} = KV.Bucket.start_link
+    {:ok, bucket: bucket}
+  end
+
+  test "stores values by key", %{bucket: bucket} do
     assert KV.Bucket.get(bucket, "milk") == nil
 
     KV.Bucket.put(bucket, "milk", 3)
     assert KV.Bucket.get(bucket, "milk") == 3
+  end
+
+  test "removes value by key", %{bucket: bucket} do
+    KV.Bucket.put(bucket, "eggs", 2)
+    assert KV.Bucket.get(bucket, "eggs") == 2
+
+    KV.Bucket.delete(bucket, "eggs")
+    assert KV.Bucket.get(bucket, "eggs") == nil
   end
 end
